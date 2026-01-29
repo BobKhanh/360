ad6e1e303f93470f711886452184b669cd4ca282
BobKhanh <bobkhanh@gmail.com>
2026-01-29 13:38:03 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,80 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20260129_104353_00_004",
+      "name": "IMG_20260129_104353_00_004",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-img_20260128_100255_00_0031",
+      "name": "IMG_20260128_100255_00_003(1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 0.13455735467683283,
+        "pitch": 0.2408320951579377,
+        "fov": 1.05050073409386
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.45918019301833723,
+          "pitch": 0.07809694660654465,
+          "rotation": 0,
+          "target": "1-img_20260128_100255_00_0031"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
